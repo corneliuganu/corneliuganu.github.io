33c7b86a5987cd88e42b46a86c25d42a57efed4f
Cornel Iuganu <yug_data@yahoo.com>
2026-03-31 10:43:29 +0300

    go

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,16 @@
 import { createRoot } from "react-dom/client";
+import "@fontsource/playfair-display/latin-400.css";
+import "@fontsource/playfair-display/latin-700.css";
+import "@fontsource/playfair-display/latin-400-italic.css";
+import "@fontsource/playfair-display/latin-ext-400.css";
+import "@fontsource/playfair-display/latin-ext-700.css";
+import "@fontsource/playfair-display/latin-ext-400-italic.css";
+import "@fontsource/raleway/latin-400.css";
+import "@fontsource/raleway/latin-500.css";
+import "@fontsource/raleway/latin-700.css";
+import "@fontsource/raleway/latin-ext-400.css";
+import "@fontsource/raleway/latin-ext-500.css";
+import "@fontsource/raleway/latin-ext-700.css";
 import App from "./App.tsx";
 import "./index.css";
 
